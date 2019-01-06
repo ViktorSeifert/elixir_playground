0dd139c3e5ca02b31c60d0a6ff2093ea03048412
Viktor Seifert <viktor@viktor-s.net>
2019-01-06 15:54:50 +0100

    Corrected formatting

--- stack_server/lib/stack_server.ex
+++ stack_server/lib/stack_server.ex
@@ -1,21 +1,22 @@
 defmodule StackServer do
   use GenServer
+
   @moduledoc """
   Documentation for StackServer.
   """
 
   @impl true
   def init(initial_stack_contents) do
-    { :ok, initial_stack_contents }
+    {:ok, initial_stack_contents}
   end
 
   @impl true
-  def handle_call(:pop, _from, _stack = [ head | tail ]) do
-    { :reply, head, tail }
+  def handle_call(:pop, _from, _stack = [head | tail]) do
+    {:reply, head, tail}
   end
 
   @impl true
-  def handle_cast({ :push, element }, stack) do
-    { :noreply, [ element | stack ] }
+  def handle_cast({:push, element}, stack) do
+    {:noreply, [element | stack]}
   end
 end
